01cee6580a8ed01cb30bd2a3cc3eb36583acc7e6
ojsc <ojsilvera@gmail.com>
2025-02-01 22:29:58 -0500

    Cambios varios

--- content.js
+++ content.js
@@ -1,7 +1,11 @@
-console.log("Twitch Side-Nav Extension: Script loaded");
-const sideNav = document.querySelector(`.side-bar-contents #side-nav .capulb div[aria-label="Followed Channels"] .InjectLayout-sc-1i43xsx-0`);
-console.log(sideNav);
-
 // To-Do
 // resolver la ejecucion al final de la carga de la pagina
 // ibtener los elementos visibles y ocultos de la lista de canales seguidos
+
+
+document.addEventListener('DOMContentLoaded',()=>{
+    console.log('La página ha terminado de cargarse!!');
+    console.log("Twitch Side-Nav Extension: Script loaded");
+    const sideNav = document.querySelector(`.side-bar-contents #side-nav .capulb div[aria-label="Followed Channels"] .InjectLayout-sc-1i43xsx-0`);
+    console.log(sideNav);
+});
